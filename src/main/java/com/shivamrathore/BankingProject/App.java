60338c60d9b3077f3409f4891b92f12d17b30f89
Shivam Rathore <r.shivam1993@gmail.com>
2018-07-09 23:24:07 +0530

    Day 3 Done with Output written in Excel

--- src/main/java/com/shivamrathore/BankingProject/App.java
+++ src/main/java/com/shivamrathore/BankingProject/App.java
@@ -1,10 +1,19 @@
 package com.shivamrathore.BankingProject;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class App extends Util {
 
 	void openBaseUrl() {
+		driver = new ChromeDriver();
+		action = new Actions(driver);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get(baseUrl);
 		driver.manage().window().maximize();
 	}
@@ -13,25 +22,52 @@ public class App extends Util {
 
 		App ob = new App();
 		ob.initialize();
-		ob.openBaseUrl();
-		if (ob.login()) {
-			System.out.println("Login Successful");
+		// ob.openBaseUrl();
+
+		if (!(ob.readFromExcel)) {
+
+			if (ob.login(ob.userName, ob.password)) {
+				System.out.println("Login Successful");
+			} else {
+				System.out.println("Loggin Unsuccessful");
+			}
 		} else {
-			System.out.println("Loggin Unsuccessful");
+			System.out.println("Excel");
+			ArrayList<Record> records = new ArrayList<Record>(ob.loadExcel());
+			for (int i = 0; i < records.size(); i++) {
+				if (ob.login(records.get(i).getUserName(), records.get(i).getPassword())) {
+					records.get(i).setResult("Pass");
+				} else {
+					records.get(i).setResult("Fail");
+				}
+			}
+			System.out.println("With Result : " + records);
+			ob.writeExcel(records);
 		}
 
 	}
 
-	private boolean login() {
+	private boolean login(String userName, String password) {
+		openBaseUrl();
 		driver.findElement(By.xpath(userIdXpath)).sendKeys(userName);
 		driver.findElement(By.xpath(passwordXpath)).sendKeys(password);
 		action.moveToElement(driver.findElement(By.xpath(loginXpath))).click().perform();
 
-		if (driver.getTitle().equalsIgnoreCase(loginTitle)) {
-			// System.out.println("Login Successful");
-			return true;
+		try {
+			if (driver.getTitle().equalsIgnoreCase(loginTitle)) {
+				// System.out.println("Login Successful");
+
+				return true;
+			} else {
+				return false;
+			}
+		} catch (UnhandledAlertException e) {
+			System.out.println("Unhandled alert at this stage usually signifies incorrect credentials (UserName : "
+					+ userName + " )");
+			return false;
+		} finally {
+			driver.quit();
 		}
 
-		return false;
 	}
 }
--- src/main/java/com/shivamrathore/BankingProject/Util.java
+++ src/main/java/com/shivamrathore/BankingProject/Util.java
@@ -1,16 +1,60 @@
 package com.shivamrathore.BankingProject;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class Util {
+	class Record {
+		String userName;
+		String password;
+		String result;
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getResult() {
+			return result;
+		}
+
+		public void setResult(String result) {
+			this.result = result;
+		}
+
+		public String getUserName() {
+			return userName;
+		}
+
+		void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+		@Override
+		public String toString() {
+			return "Record [userName=" + userName + ", password=" + password + ", result=" + result + "]";
+		}
+
+	}
 
 	WebDriver driver;
 	Actions action;
@@ -21,15 +65,87 @@ public class Util {
 	String userIdXpath;
 	String passwordXpath;
 	String loginXpath;
-
+	boolean readFromExcel;
+	String excelFile;
 	final String loginTitle = "Guru99 Bank Manager HomePage";
 
 	void initialize() {
+
 		loadConfiguration();
 		System.setProperty("webdriver.chrome.driver", driverLocation);
-		driver = new ChromeDriver();
-		action = new Actions(driver);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+	List<Record> loadExcel() {
+		List<Record> records = new ArrayList<Record>();
+		Record record = null;
+		FileInputStream file;
+		Workbook workbook = null;
+		try {
+			file = new FileInputStream(new File(excelFile));
+			workbook = new XSSFWorkbook(file);
+			Sheet sheet = workbook.getSheetAt(0);
+			for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+				Row row = sheet.getRow(i);
+				record = new Record();
+				record.setUserName(row.getCell(0).toString());
+				record.setPassword(row.getCell(1).toString());
+				System.out.println(record);
+
+				records.add(record);
+			}
+
+			System.out.println("Load Excel returns : " + records);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				workbook.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return records;
+	}
+
+	void writeExcel(ArrayList<Record> records) {
+
+		FileInputStream file;
+		XSSFWorkbook workbook = null;
+		try {
+			file = new FileInputStream(new File(excelFile));
+			workbook = new XSSFWorkbook(file);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+				Row row = sheet.getRow(i);
+
+				Cell cell = row.createCell(2);
+				cell.setCellValue(records.get(i - 1).getResult());
+
+			}
+
+			file.close();
+
+			FileOutputStream outFile = new FileOutputStream(excelFile);
+			workbook.write(outFile);
+			workbook.close();
+			outFile.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	boolean loadConfiguration() {
@@ -47,7 +163,10 @@ public class Util {
 			userIdXpath = props.getProperty("UserID_xpath");
 			passwordXpath = props.getProperty("Password_xpath");
 			loginXpath = props.getProperty("Login_xpath");
-
+			excelFile = props.getProperty("Input_Excel");
+			if (props.getProperty("Read_From_Excel").equalsIgnoreCase("Y")) {
+				readFromExcel = true;
+			}
 			// System.out.println("URL : " + baseUrl);
 			// System.out.println("User : " + userName);
 			// System.out.println("Password : " + password);
